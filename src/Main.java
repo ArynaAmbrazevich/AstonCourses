3c47f61f8f3c6affdd3ca0d78cb1ba8a5d6143c6
Olga Sinegub <volgasinegub@gmail.com>
2024-10-25 20:03:09 +0200

    Complete task 4

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,63 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
         printThreeWords();
+        System.out.println();
+
         checkSumSign();
+        System.out.println();
+
         printColor();
+        System.out.println();
+
         compareNumbers();
+        System.out.println();
+
         System.out.println(checkNumbersSum(2, 13));
+        System.out.println();
+
         checkPositiveOrNegative(5);
+        System.out.println();
+
         System.out.println(checkBooleanPositiveOrNegative(6));
-        changeZeroToOneArray();
-        fillArray();
-        mulArrayElements();
+        System.out.println();
+
         printStringForLoop("What?", 5);
+        System.out.println();
+
+        changeZeroToOneArray();
+        System.out.println();
+        System.out.println();
+
+        fillArray();
+        System.out.println();
+        System.out.println();
+
+        mulArrayElements();
+        System.out.println();
+        System.out.println();
+
         int[] newArray = createArray(5, 12);
         for (Integer num : newArray) {
             System.out.println(num);
         }
+        System.out.println();
+
         System.out.println(isLeapYear(2016));
+        System.out.println();
+
         fillInMatrix();
     }
 
     public static void printThreeWords() {
+        System.out.println("Первое задание:");
         System.out.println("Orange");
         System.out.println("Banana");
         System.out.println("Apple");
     }
 
     public static void checkSumSign() {
+        System.out.println("Второе задание");
         int a = 5;
         int b = 8;
         int result = a + b;
@@ -39,6 +69,7 @@ public class Main {
     }
 
     public static void printColor() {
+        System.out.println("Третье задание");
         int value = -5;
         if (value <= 0) {
             System.out.println("красный");
@@ -52,6 +83,7 @@ public class Main {
     }
 
     public static void compareNumbers() {
+        System.out.println("Четвертое задание");
         int a = 2;
         int b = 2;
         if (a >= b) {
@@ -63,6 +95,7 @@ public class Main {
     }
 
     public static boolean checkNumbersSum(int a, int b) {
+        System.out.println("Пятое задание");
         int result = a + b;
         if (result >= 10 && result <= 20) {
             return true;
@@ -72,6 +105,7 @@ public class Main {
     }
 
     public static void checkPositiveOrNegative(int a) {
+        System.out.println("Шестое задание");
         if (a >= 0) {
             System.out.println("число положительное");
         }
@@ -81,6 +115,7 @@ public class Main {
     }
 
     public static boolean checkBooleanPositiveOrNegative(int a) {
+        System.out.println("Седьмое задание");
         boolean isPositive = false;
         if (a >= 0) {
             isPositive = false;
@@ -92,12 +127,14 @@ public class Main {
     }
 
     public static void printStringForLoop(String anyString, int number) {
+        System.out.println("Восьмое задание");
         for (int i = 0; i < number; i++) {
             System.out.println(anyString);
         }
     }
 
     public static boolean isLeapYear(int year) {
+        System.out.println("Девятое задание");
         boolean isLeap = false;
         if (year % 4 == 0) {
             if ((year % 100 != 0) || (year % 400 == 0)) {
@@ -108,6 +145,7 @@ public class Main {
     }
 
     public static void changeZeroToOneArray() {
+        System.out.println("Десятое задание");
         int[] initArray = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
         for (int i = 0; i < initArray.length; i++) {
             if (initArray[i] == 1) {
@@ -122,17 +160,18 @@ public class Main {
     }
 
     public static void fillArray() {
+        System.out.println("Одиннадцатое задание");
         int[] initArray = new int[100];
         for (int i = 0; i < initArray.length; i++) {
             initArray[i] = i + 1;
         }
-        System.out.println();
         for (Integer in : initArray) {
-            System.out.println(in);
+            System.out.print(in);
         }
     }
 
     public static void mulArrayElements() {
+        System.out.println("Двенадцатое задание");
         int[] initArray = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
         for (int i = 0; i < initArray.length; i++) {
             if (initArray[i] < 6) {
@@ -145,6 +184,7 @@ public class Main {
     }
 
     public static void fillInMatrix() {
+        System.out.println("Тринадцатое задание");
         int n = 5;
         int[][] array = new int[n][n];
         for (int i = 0; i < n; i++) {
@@ -160,6 +200,7 @@ public class Main {
     }
 
     public static int[] createArray(int len, int initialValue) {
+        System.out.println("Четырнадцатое задание");
         int[] initArray = new int[len];
         for (int i = 0; i < initArray.length; i++) {
             initArray[i] = initialValue;
